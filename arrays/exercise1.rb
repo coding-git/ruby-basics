ea5b7d593227da8d02de246f6df6fe433c4ddf6a
coding-git <38887449+coding-git@users.noreply.github.com>
2018-05-08 10:08:47 -0400

    First commit

---
+++ arrays/exercise1.rb
@@ -0,0 +1,6 @@
+# Arrays - exercise 1
+
+arr = [1,3,5, 7, 9, 11]
+num = 3
+
+arr.include?(num)
---
+++ arrays/exercise7.rb
@@ -0,0 +1,8 @@
+# Arrays - Exercise 7
+
+orig_arr = [1, 3, 5, 7]
+incremented_arr = orig_arr.map { |val| val+2 }
+
+p "Original array: #{orig_arr}"
+p "Incremented array: #{incremented_arr}"
+
